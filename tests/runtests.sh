a3009898c53b3a95e875d8ef89ecdcc90b69f21d
CodePi <CodePi@users.noreply.github.com>
2021-02-09 11:08:20 -0500

    added eigen_units_test to runtests.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,4 +2,12 @@
 
 set -e
 
+echo --------------------------
+echo units_test
 ./units_test
+
+if test -f "./eigen_units_test"; then
+    echo --------------------------
+    echo eigen_units_test
+    ./eigen_units_test
+fi
