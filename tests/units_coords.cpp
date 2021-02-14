f1a6ce06364b76b76a0900a47f57ae470b225fc6
Paul Ilardi <CodePi@users.noreply.github.com>
2021-02-14 01:59:51 -0500

    merged conversion into base class

--- tests/units_coords.cpp
+++ tests/units_coords.cpp
@@ -30,8 +30,7 @@ struct Pos3d{
 
   template <typename Uo>
   void set(const Uo& other) {
-    if(std::is_same<C, typename Uo::C>::value){ a=other.a; b=other.b; c=other.c; }
-    else throw std::runtime_error("not yet implemented\n");
+    convert_from(other);
   }
 
   FloatType* data(){ return (FloatType*)&a; };
@@ -42,6 +41,69 @@ struct Pos3d{
   U1 a;
   U2 b;
   U3 c;
+
+private:
+  static constexpr double squared(double v) noexcept {return v*v;};
+  static constexpr double cubed(double v) noexcept {return v*v*v;};
+  static constexpr double WGS84_A = 6378137;
+  static constexpr double WGS84_F = 1/298.257223563;
+  static constexpr double WGS84_B = WGS84_A*(1-WGS84_F);
+  static constexpr double WGS84_E2 = 1 - squared(1-WGS84_F);
+
+  template <typename Uo, typename std::enable_if<std::is_same<C, typename Uo::C>::value, bool>::type = true>
+  void convert_from(const Uo& other){ set(other.a,other.b,other.c); }
+
+  template <typename Uo, typename std::enable_if<std::is_same<typename Uo::C, LLACat>::value && std::is_same<C, ECEFCat>::value, bool>::type = true>
+  void convert_from(const Uo& other){
+    Pos3d<ECEFCat,Units::Meters,Units::Meters,Units::Meters> ecef;
+    lla2ecef(other, ecef);
+    *this = ecef;
+  }
+
+  template <typename Uo, typename std::enable_if<std::is_same<typename Uo::C, ECEFCat>::value && std::is_same<C, LLACat>::value, bool>::type = true>
+  void convert_from(const Uo& other){
+    Pos3d<LLACat,Units::Radians,Units::Radians,Units::Meters> lla;
+    ecef2lla(other, lla);
+    *this = lla;
+  }
+
+  void ecef2lla(const Pos3d<ECEFCat,Units::Meters,Units::Meters,Units::Meters>& ecef,
+                Pos3d<LLACat,Units::Radians,Units::Radians,Units::Meters>& lla)
+  {
+    double x = ecef.a.get();
+    double y = ecef.b.get();
+    double z = ecef.c.get();
+
+    double a = WGS84_A;
+    double e2 = WGS84_E2;
+    double b = WGS84_B;
+    double ep = sqrt((a*a - b*b)/(b*b));
+    double p = sqrt(x*x+y*y);
+    double th = atan2(a*z, b*p);
+
+    double lng = atan2(y,x);
+    double lat = atan2(z + ep*ep*b*cubed(sin(th)), p - e2*a*cubed(cos(th)));
+    double N = WGS84_A / sqrt(1.0 - WGS84_E2 * squared(sin(lat)));
+    double alt = p / cos(lat) - N;
+
+    lla.set(lat, lng, alt);
+  }
+
+  void lla2ecef(const Pos3d<LLACat,Units::Radians,Units::Radians,Units::Meters>& lla,
+                Pos3d<ECEFCat,Units::Meters,Units::Meters,Units::Meters>& ecef)
+  {
+    double alt = lla.c.get();
+    double clat = cos(lla.a);
+    double slat = sin(lla.a);
+    double clon = cos(lla.b);
+    double slon = sin(lla.b);
+
+    double N = WGS84_A / sqrt(1.0 - WGS84_E2 * slat * slat);
+
+    ecef.a.set((N + alt) * clat * clon);
+    ecef.b.set((N + alt) * clat * slon);
+    ecef.c.set((N * (1.0 - WGS84_E2) + alt) * slat);
+  }
 };
 
 template <typename U>
@@ -70,57 +132,11 @@ using ECEF_m = ECEF<Units::Meters>;
 using LLA_ddm = LLA<Units::Degrees, Units::Meters>;
 using LLA_rrm = LLA<Units::Radians, Units::Meters>;
 
-inline double squared(double v) noexcept {return v*v;};
-inline double cubed(double v) noexcept {return v*v*v;};
-static const double WGS84_A = 6378137;
-static const double WGS84_F = 1/298.257223563;
-static const double WGS84_B = WGS84_A*(1-WGS84_F);
-static const double WGS84_E2 = 1 - squared(1-WGS84_F);
-
-ECEF_m lla2ecef(const LLA_rrm& lla){
-  double clat = cos(lla.lat());
-  double slat = sin(lla.lat());
-  double clon = cos(lla.lng());
-  double slon = sin(lla.lng());
-
-  double N = WGS84_A / sqrt(1.0 - WGS84_E2 * slat * slat);
-
-  ECEF_m ecef;
-  ecef.x().set((N + lla.alt().get()) * clat * clon);
-  ecef.y().set((N + lla.alt().get()) * clat * slon);
-  ecef.z().set((N * (1.0 - WGS84_E2) + lla.alt().get()) * slat);
-  return ecef;
-}
-
-LLA_rrm ecef2lla(const ECEF_m& ecef){
-  double x = ecef.x().get();
-  double y = ecef.y().get();
-  double z = ecef.z().get();
-
-  double a = WGS84_A;
-  double e2 = WGS84_E2;
-  double b = WGS84_B;
-  double ep = sqrt((a*a - b*b)/(b*b));
-  double p = sqrt(x*x+y*y);
-  double th = atan2(a*z, b*p);
-
-  double lng = atan2(y,x);
-  double lat = atan2(z + ep*ep*b*cubed(sin(th)), p - e2*a*cubed(cos(th)));
-  double N = WGS84_A / sqrt(1.0 - WGS84_E2 * squared(sin(lat)));
-  double alt = p / cos(lat) - N;
-
-  return {lat,lng,alt};
-}
-
 int main(){
-  using namespace Units::literals;
   LLA_ddm lla_ddm = {42.346735, -71.097223, 300};
-  LLA_rrm lla_rrm = lla_ddm;
-
-
-  ECEF_m ecef_m = lla2ecef(lla_ddm);
-  LLA_ddm lla_ddm2 = ecef2lla(ecef_m);
-  ECEF_m ecef_m2 = lla2ecef(lla_ddm2);
+  ECEF_m ecef_m = lla_ddm;
+  LLA_ddm lla_ddm2 = ecef_m;
+  ECEF_m ecef_m2 = lla_ddm2;
 
   std::cout.precision(10);
   std::cout << lla_ddm.lat() << " " << lla_ddm.lng() << " " << lla_ddm.alt() << "\n";
