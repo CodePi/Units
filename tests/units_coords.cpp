87d373a015c31fc2525780ea2162e71beec089e2
Paul Ilardi <CodePi@users.noreply.github.com>
2021-02-14 01:08:32 -0500

    implemented ecef2lla

--- tests/units_coords.cpp
+++ tests/units_coords.cpp
@@ -6,58 +6,64 @@
 #include <iostream>
 #include <iomanip>
 
-template <typename U=Units::Meters>
-struct ECEF{
-  U x;
-  U y;
-  U z;
+struct ECEFCat{};
+struct LLACat{};
+struct ENUCat{};
 
-  using FloatType = typename U::FloatType;
+template <typename C_, typename U1, typename U2, typename U3>
+struct Pos3d{
+  using FloatType = typename U1::FloatType;
+  using C = C_;
 
-  ECEF() = default;
-  ECEF(FloatType x_, FloatType y_, FloatType z_) { set(x_,y_,z_); }
+  Pos3d()=default;
 
-  template <typename U2>
-  ECEF(const ECEF<U2>& ecef2) { set(ecef2); }
+  template <typename Uo>
+  Pos3d(const Uo& other) { set(other); }
 
-  template <typename U2>
-  ECEF operator=(const ECEF<U2>& ecef2) { set(ecef2); return *this; }
+  Pos3d(FloatType a,FloatType b,FloatType c){ set(a,b,c); }
 
-  void set(FloatType x_, FloatType y_, FloatType z_){ x.set(x_); y.set(y_); z.set(z_); }
+  template <typename Uo>
+  Pos3d operator=(const Uo& ecef2) { set(ecef2); return *this; }
 
-  template <typename U2>
-  void set(const ECEF<U2>& ecef2) { x=ecef2.x; y=ecef2.y; z=ecef2.z; };
+  void set(FloatType a_, FloatType b_, FloatType c_){ a.set(a_); b.set(b_); c.set(c_); }
+  void set(U1 a_, U2 b_, U3 c_){ a.set(a_); b.set(b_); c.set(c_); }
 
-  FloatType* data(){ return (FloatType*)this; };
+  template <typename Uo>
+  void set(const Uo& other) {
+    if(std::is_same<C, typename Uo::C>::value){ a=other.a; b=other.b; c=other.c; }
+    else throw std::runtime_error("not yet implemented\n");
+  }
 
-  operator U*(){ return (U*)this; }
+  FloatType* data(){ return (FloatType*)&a; };
+
+  static_assert(std::is_same<typename U1::FloatType,typename U2::FloatType>::value,"");
+  static_assert(std::is_same<typename U1::FloatType,typename U3::FloatType>::value,"");
+
+  U1 a;
+  U2 b;
+  U3 c;
+};
+
+template <typename U>
+struct ECEF : public Pos3d<ECEFCat,U,U,U> {
+  using Pos3d<ECEFCat,U,U,U>::Pos3d;
+  U& x(){return this->a;}
+  U& y(){return this->b;}
+  U& z(){return this->c;}
+  const U& x() const{return this->a;}
+  const U& y() const{return this->b;}
+  const U& z() const{return this->c;}
 };
 
 template <typename Ull, typename Ua>
-struct LLA{
-  Ull lat;
-  Ull lng;
-  Ua  alt;
-
-  static_assert(std::is_same<typename Ull::FloatType, typename Ua::FloatType>::value,"");
-
-  using FloatType = typename Ull::FloatType;
-
-  LLA(FloatType lat_, FloatType lng_, FloatType alt_) { set(lat_, lng_, alt_); }
-
-  template <typename Ull2, typename Ua2>
-  LLA(const LLA<Ull2, Ua2>& lla2) { set(lla2); }
-
-  template <typename Ull2, typename Ua2>
-  LLA operator=(const LLA<Ull2, Ua2>& lla2) { set(lla2); return *this; }
-
-  void set(FloatType lat_, FloatType lng_, FloatType alt_){ lat.set(lat_); lng.set(lng_); alt.set(alt_); }
-
-  template <typename Ull2, typename Ua2>
-  void set(const LLA<Ull2, Ua2>& lla2) { lat=lla2.lat; lng=lla2.lng; alt=lla2.alt; };
-
-  FloatType* data(){ return (FloatType*)this; };
-
+struct LLA : public Pos3d<LLACat,Ull,Ull,Ua>{
+  using Pos3d<LLACat,Ull,Ull,Ua>::Pos3d;
+  Ull& lat(){return this->a;}
+  Ull& lng(){return this->b;}
+  Ua& alt(){return this->c;}
+  const Ull& lat() const{return this->a;}
+  const Ull& lng() const{return this->b;}
+  const Ua& alt() const{return this->c;}
 };
 
 using ECEF_m = ECEF<Units::Meters>;
@@ -72,24 +78,24 @@ static const double WGS84_B = WGS84_A*(1-WGS84_F);
 static const double WGS84_E2 = 1 - squared(1-WGS84_F);
 
 ECEF_m lla2ecef(const LLA_rrm& lla){
-  double clat = cos(lla.lat);
-  double slat = sin(lla.lat);
-  double clon = cos(lla.lng);
-  double slon = sin(lla.lng);
+  double clat = cos(lla.lat());
+  double slat = sin(lla.lat());
+  double clon = cos(lla.lng());
+  double slon = sin(lla.lng());
 
   double N = WGS84_A / sqrt(1.0 - WGS84_E2 * slat * slat);
 
   ECEF_m ecef;
-  ecef.x.set((N + lla.alt.get()) * clat * clon);
-  ecef.y.set((N + lla.alt.get()) * clat * slon);
-  ecef.z.set((N * (1.0 - WGS84_E2) + lla.alt.get()) * slat);
+  ecef.x().set((N + lla.alt().get()) * clat * clon);
+  ecef.y().set((N + lla.alt().get()) * clat * slon);
+  ecef.z().set((N * (1.0 - WGS84_E2) + lla.alt().get()) * slat);
   return ecef;
 }
 
 LLA_rrm ecef2lla(const ECEF_m& ecef){
-  double x = ecef.x.get();
-  double y = ecef.y.get();
-  double z = ecef.z.get();
+  double x = ecef.x().get();
+  double y = ecef.y().get();
+  double z = ecef.z().get();
 
   double a = WGS84_A;
   double e2 = WGS84_E2;
@@ -98,24 +104,27 @@ LLA_rrm ecef2lla(const ECEF_m& ecef){
   double p = sqrt(x*x+y*y);
   double th = atan2(a*z, b*p);
 
-  double lon = atan2(y,x);
+  double lng = atan2(y,x);
   double lat = atan2(z + ep*ep*b*cubed(sin(th)), p - e2*a*cubed(cos(th)));
   double N = WGS84_A / sqrt(1.0 - WGS84_E2 * squared(sin(lat)));
   double alt = p / cos(lat) - N;
 
-  return {lat, lon, alt};
+  return {lat,lng,alt};
 }
 
 int main(){
   using namespace Units::literals;
   LLA_ddm lla_ddm = {42.346735, -71.097223, 300};
+  LLA_rrm lla_rrm = lla_ddm;
+
+
   ECEF_m ecef_m = lla2ecef(lla_ddm);
   LLA_ddm lla_ddm2 = ecef2lla(ecef_m);
   ECEF_m ecef_m2 = lla2ecef(lla_ddm2);
 
   std::cout.precision(10);
-  std::cout << lla_ddm.lat << " " << lla_ddm.lng << " " << lla_ddm.alt << "\n";
-  std::cout << ecef_m.x << " " << ecef_m.y << " " << ecef_m.z << "\n";
-  std::cout << lla_ddm2.lat << " " << lla_ddm2.lng << " " << lla_ddm2.alt << "\n";
-  std::cout << ecef_m2.x << " " << ecef_m2.y << " " << ecef_m2.z << "\n";
+  std::cout << lla_ddm.lat() << " " << lla_ddm.lng() << " " << lla_ddm.alt() << "\n";
+  std::cout << ecef_m.x() << " " << ecef_m.y() << " " << ecef_m.z() << "\n";
+  std::cout << lla_ddm2.lat() << " " << lla_ddm2.lng() << " " << lla_ddm2.alt() << "\n";
+  std::cout << ecef_m2.x() << " " << ecef_m2.y() << " " << ecef_m2.z() << "\n";
 }
