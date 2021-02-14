8799c36acc2100e60c2c96aabc6a400192058916
Paul Ilardi <CodePi@users.noreply.github.com>
2021-02-14 02:13:43 -0500

    renamed variables

--- tests/units_coords.cpp
+++ tests/units_coords.cpp
@@ -25,22 +25,22 @@ struct Pos3d{
   template <typename Uo>
   Pos3d operator=(const Uo& ecef2) { set(ecef2); return *this; }
 
-  void set(FloatType a_, FloatType b_, FloatType c_){ a.set(a_); b.set(b_); c.set(c_); }
-  void set(U1 a_, U2 b_, U3 c_){ a.set(a_); b.set(b_); c.set(c_); }
+  void set(FloatType a_, FloatType b_, FloatType c_){ v1.set(a_); v2.set(b_); v3.set(c_); }
+  void set(U1 a_, U2 b_, U3 c_){ v1.set(a_); v2.set(b_); v3.set(c_); }
 
   template <typename Uo>
   void set(const Uo& other) {
     convert_from(other);
   }
 
-  FloatType* data(){ return (FloatType*)&a; };
+  FloatType* data(){ return (FloatType*)&v1; };
 
   static_assert(std::is_same<typename U1::FloatType,typename U2::FloatType>::value,"");
   static_assert(std::is_same<typename U1::FloatType,typename U3::FloatType>::value,"");
 
-  U1 a;
-  U2 b;
-  U3 c;
+  U1 v1;
+  U2 v2;
+  U3 v3;
 
 private:
   static constexpr double squared(double v) noexcept {return v*v;};
@@ -51,7 +51,7 @@ private:
   static constexpr double WGS84_E2 = 1 - squared(1-WGS84_F);
 
   template <typename Uo, typename std::enable_if<std::is_same<C, typename Uo::C>::value, bool>::type = true>
-  void convert_from(const Uo& other){ set(other.a,other.b,other.c); }
+  void convert_from(const Uo& other){ set(other.v1,other.v2,other.v3); }
 
   template <typename Uo, typename std::enable_if<std::is_same<typename Uo::C, LLACat>::value && std::is_same<C, ECEFCat>::value, bool>::type = true>
   void convert_from(const Uo& other){
@@ -70,9 +70,9 @@ private:
   void ecef2lla(const Pos3d<ECEFCat,Units::Meters,Units::Meters,Units::Meters>& ecef,
                 Pos3d<LLACat,Units::Radians,Units::Radians,Units::Meters>& lla)
   {
-    double x = ecef.a.get();
-    double y = ecef.b.get();
-    double z = ecef.c.get();
+    double x = ecef.v1.get();
+    double y = ecef.v2.get();
+    double z = ecef.v3.get();
 
     double a = WGS84_A;
     double e2 = WGS84_E2;
@@ -92,40 +92,40 @@ private:
   void lla2ecef(const Pos3d<LLACat,Units::Radians,Units::Radians,Units::Meters>& lla,
                 Pos3d<ECEFCat,Units::Meters,Units::Meters,Units::Meters>& ecef)
   {
-    double alt = lla.c.get();
-    double clat = cos(lla.a);
-    double slat = sin(lla.a);
-    double clon = cos(lla.b);
-    double slon = sin(lla.b);
+    double alt = lla.v3.get();
+    double clat = cos(lla.v1);
+    double slat = sin(lla.v1);
+    double clon = cos(lla.v2);
+    double slon = sin(lla.v2);
 
     double N = WGS84_A / sqrt(1.0 - WGS84_E2 * slat * slat);
 
-    ecef.a.set((N + alt) * clat * clon);
-    ecef.b.set((N + alt) * clat * slon);
-    ecef.c.set((N * (1.0 - WGS84_E2) + alt) * slat);
+    ecef.v1.set((N + alt) * clat * clon);
+    ecef.v2.set((N + alt) * clat * slon);
+    ecef.v3.set((N * (1.0 - WGS84_E2) + alt) * slat);
   }
 };
 
 template <typename U>
 struct ECEF : public Pos3d<ECEFCat,U,U,U> {
   using Pos3d<ECEFCat,U,U,U>::Pos3d;
-  U& x(){return this->a;}
-  U& y(){return this->b;}
-  U& z(){return this->c;}
-  const U& x() const{return this->a;}
-  const U& y() const{return this->b;}
-  const U& z() const{return this->c;}
+  U& x(){return this->v1;}
+  U& y(){return this->v2;}
+  U& z(){return this->v3;}
+  const U& x() const{return this->v1;}
+  const U& y() const{return this->v2;}
+  const U& z() const{return this->v3;}
 };
 
 template <typename Ull, typename Ua>
 struct LLA : public Pos3d<LLACat,Ull,Ull,Ua>{
   using Pos3d<LLACat,Ull,Ull,Ua>::Pos3d;
-  Ull& lat(){return this->a;}
-  Ull& lng(){return this->b;}
-  Ua& alt(){return this->c;}
-  const Ull& lat() const{return this->a;}
-  const Ull& lng() const{return this->b;}
-  const Ua& alt() const{return this->c;}
+  Ull& lat(){return this->v1;}
+  Ull& lng(){return this->v2;}
+  Ua& alt(){return this->v3;}
+  const Ull& lat() const{return this->v1;}
+  const Ull& lng() const{return this->v2;}
+  const Ua& alt() const{return this->v3;}
 };
 
 using ECEF_m = ECEF<Units::Meters>;
@@ -143,4 +143,8 @@ int main(){
   std::cout << ecef_m.x() << " " << ecef_m.y() << " " << ecef_m.z() << "\n";
   std::cout << lla_ddm2.lat() << " " << lla_ddm2.lng() << " " << lla_ddm2.alt() << "\n";
   std::cout << ecef_m2.x() << " " << ecef_m2.y() << " " << ecef_m2.z() << "\n";
+
+  // wtf
+  LLA<Units::Radians, Units::Furlongs> wtf = ecef_m;
+  std::cout << wtf.lat() << " " << wtf.lng() << " " << wtf.alt() << "\n";
 }
