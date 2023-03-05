f3d333c31385a2a7cbfd425c923a5b6e47ae5ac7
Paul Ilardi <CodePi@users.noreply.github.com>
2023-03-05 01:32:23 -0500

    added beginnings of C++20 version of units

---
+++ tests/units20_test.cpp
@@ -0,0 +1,321 @@
+// Copyright (C) 2021 Paul Ilardi (http://github.com/CodePi)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, unconditionally.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#if __cplusplus < 202000
+#include <iostream>
+int main(){
+  std::cout << "C++20 not supported by this compiler\n";
+}
+#else
+
+#include "../units20.h"
+
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+using namespace Units;
+using namespace Units::literals;
+
+bool approx(double a, double b, double epsilon=1e-6){
+  return fabs(a-b) <= epsilon;
+}
+
+// if you don't know the unit at compile time, you can use something like this to convert it
+Meters ToMeters(double val, const string& unit){
+  if(unit=="m") return Meters(val);
+  if(unit=="ft") return Feet(val);
+  if(unit=="mi") return Miles(val);
+  throw std::runtime_error("ToMeters: unit not recognized");
+}
+
+// parses string of format number followed by unit, spaces are ignored and anything past units is ignored
+// e.g. "5ft", "2.7m", "  27 ft  ", "3.14ft this is ignored"
+Meters ToMeters(const string& val){
+  stringstream ss(val);
+  double dval;
+  string unit;
+  ss >> dval >> unit;
+  if(!ss) throw std::runtime_error("ToMeters: error parsing string: "+val);
+  return ToMeters(dval, unit);
+}
+
+int main(){
+  Kilometers km1(5);
+  assert(km1.get<Meters>()==5000);
+  assert(km1.get<Centimeters>()==500000);
+
+  Centimeters cm1 = km1;
+  assert(cm1.get<Kilometers>()==5);
+  assert(cm1.get<Centimeters>()==500000);
+
+  Kilometers km2;
+  km2.set<Centimeters>(500000);
+  assert(km2.get<Kilometers>()==5);
+  assert(km2.get<Centimeters>()==500000);
+
+  Centimeters cm2;
+  cm2.set<Kilometers>(5);
+  assert(cm2.get<Meters>()==5000);
+  assert(cm2.get<Centimeters>()==500000);
+
+  Kilometers km3;
+  km3 = cm2;
+  assert(km3.get<Kilometers>()==5);
+  assert(km3.get<Centimeters>()==500000);
+
+  Kilometers km4 = km3+cm2;
+  assert(km4.get<Kilometers>()==10);
+  assert(km4.get<Centimeters>()==1000000);
+
+  Centimeters cm4 = km3+cm2;
+  assert(cm4.get<Kilometers>()==10);
+  assert(cm4.get<Centimeters>()==1000000);
+
+  Centimeters cm5 = cm4*2;
+  assert(cm5.get<Meters>()==20000);
+
+  Centimeters cm6 = cm5/2+km4*2;
+  assert(cm6 == Kilometers(30));
+
+  double ratio = km4/cm1;
+  assert(ratio==2);
+
+  assert(cm1==km1);
+  assert(cm1!=km4);
+  assert(cm1<km4);
+  assert(cm1<=cm1);
+  assert(cm1<=km4);
+
+  Milliseconds ms(100);
+  assert(ms.get<Microseconds>()==100*1000);
+
+  // print out conversions
+  cout << "Inches in a meter: " << Meters(1).get<Inches>() << "\n";
+  cout << "Inches in a foot: " << Feet(1).get<Inches>() << "\n";
+  cout << "Feet in a meter: " << Meters(1).get<Feet>() << "\n";
+  cout << "Yards in a meter: " << Meters(1).get<Yards>() << "\n";
+  cout << "Feet in a yard: " << Yards(1).get<Feet>() << "\n";
+  cout << "Meters in a mile: " << Miles(1).get<Meters>() << "\n";
+  cout << "Feet in a mile: " << Miles(1).get<Feet>() << "\n";
+  cout << "Yards in a mile: " << Miles(1).get<Yards>() << "\n";
+  cout << "Centimeters in an inch: " << Inches(1).get<Centimeters>() << "\n";
+  cout << "Meters in a nautical mile: " << NauticalMiles(1).get<Meters>() << "\n";
+  cout << "120 KPH to MPH: " << KilometersPerHour(120).get<MPH>() << "\n";
+  cout << "100 knots to mps: " << Knots(100).get<MetersPerSecond>() << "\n";
+  cout << "Speed of sound in MPH: " << Mach(1).get<MPH>() << "\n";
+  cout << "Square Feet in an Square Meter: " << SquareMeters(1).get<SquareFeet>() << "\n";
+  cout << "Square Feet in an Acre: " << Acres(1).get<SquareFeet>() << "\n";
+  cout << "CubicCentimeters in a Liter: " << Liters(1).get<CubicCentimeters>() << "\n";
+  cout << "Liters in a Gallon: " << Gallons(1).get<Liters>() << "\n";
+  cout << "Quarts in a Gallon: " << Gallons(1).get<Quarts>() << "\n";
+  cout << "Pints in a Gallon: " << Gallons(1).get<Pints>() << "\n";
+  cout << "Cups in a Gallon: " << Gallons(1).get<Cups>() << "\n";
+  cout << "FluidOunces in a Gallon: " << Gallons(1).get<FluidOunces>() << "\n";
+  cout << "CubicInches in a Gallon: " << Gallons(1).get<CubicInches>() << "\n";
+  cout << "CubicInches in a Liter: " << Liters(1).get<CubicInches>() << "\n";
+  cout << "SquareInches in a SquareMeter: " << SquareMeters(1).get<SquareInches>() << "\n";
+  cout << "Pounds in a Kilogram: " << Kilograms(1).get<Pounds>() << "\n";
+  //cout << "40 C to F: " << Celsius(40).get<Fahrenheit>() << "\n";
+  //cout << "0 C to F: " << Celsius(0).get<Fahrenheit>() << "\n";
+  //cout << "-40 C to F: " << Celsius(-40).get<Fahrenheit>() << "\n";
+  //cout << "0 C to K: " << Celsius(0).get<Kelvin>() << "\n";
+  cout << "180 deg in radians: " << Degrees(180).get<Radians>() << "\n";
+  cout << "40 rods to the hogshead to miles per gallon: " << RodsToTheHogHead(40).get<MilesPerGallon>() << "\n";
+  cout << "1 lightyear in meters: " << LightYears(1).get<Meters>() << "\n";
+
+  // user-defined literals
+  Meters m = 10_m + 15_cm;
+  assert(m == 10.15_m);
+
+  // Test unit conversions
+  assert(1_in == 2.54_cm);
+  assert(12_in == 1_ft);
+  assert(1_yd == 3_ft);
+  assert(381_m == 1250_ft);
+  assert(1_mi == 5280_ft);
+  assert(1_nm == Angstroms(10));
+  assert(NauticalMiles(1)==1852_m);
+  assert(Knots(900)==MetersPerSecond(463));
+  assert(Mach(1)==MetersPerSecond(343));
+  assert(Knots(1)==1.852_kph);
+  assert(Gallons(1)==FluidOunces(128));
+  assert(Gallons(1).approx(Liters(3.78541)));
+  assert(Acres(1)==SquareFeet(43560));
+  assert(SquareMeters(145161)==SquareFeet(1562500));
+  assert(Milliliters(1)==CubicCentimeters(1));
+  assert(Gallons(1)==CubicInches(231));
+  assert(Liters(2048383)==CubicInches(125000000));
+  assert(CubicMeters(1)==Liters(1000));
+  assert(MetersPerSecondSquared(381)==FeetPerSecondSquared(1250));
+  assert(45359.237_kg == 100000_lbs);
+  assert(16_oz == 1_lbs);
+  assert(PoundsForce(1).approx(4.44822_N));
+  //assert(40_C == 104_F);
+  //assert(0_C == 32_F);
+  //assert(0_C == 273.15_K);
+  //assert(Celsius(-40) == Fahrenheit(-40));
+  //assert(Rankine(0)==0_K);
+  //assert(Rankine(459.67)==0_F);
+  assert((1_mpg).approx(KilometersPerLiter(0.425143707)));
+  assert(Rods(1)==16.5_ft);
+  assert(Gallons(63)==HogsHead(1));
+  assert(RodsToTheHogHead(40).approx(MilesPerGallon(0.001984127)));
+  assert(1000_kg == MetricTons(1));
+  assert(2000_lbs == Tons(1));
+  assert(MetricTons(1).approx(Tons(1.1023113)));
+  assert(Bananas(1)==178_mm);
+  assert(SpeedOfLight(1) == MetersPerSecond(299792458));
+  assert(LightYears(1).approx(Meters(9.46053e+15)));
+  assert(LightYears(10) == AstronomicalUnits(632411));
+  assert(Parsecs(1) == AstronomicalUnits(206265));
+  assert(86400_s == 1_dy);
+  assert(220_yd == Furlongs(1));
+  assert(Fortnights(1)==14_dy);
+  assert(SquareMiles(1)==Acres(640));
+  assert(Pounds(14) == Stones(1));
+/*
+  // speed = distance / time
+  MPH mph = 100_mi / 8_hr;
+  assert(mph == 12.5_mph);
+  Knots knots = 1234.5_ft / 10_wk;
+
+  // Energy and power
+  assert(WattHours(1) == Joules(3600));
+  assert(Watts(6000)*Minutes(30) == KilowattHours(3));
+  assert((1.21_GW).approx(1985_MJ/(2015_in/88_mph)/1.260942));
+  assert(10_V*120_A == 1.2_KW);
+  assert(10_mA * 20_ohms == 200_mV);
+*/
+  // frequency
+  assert(1_KHz==1000_Hz);
+  assert(1_MHz==1000_KHz);
+  assert(1_GHz==1000_MHz);
+  assert(1_THz==1000_GHz);
+/*
+  // frequency to interval conversion
+  assert(inverse(1_Hz)==1_s);
+  assert(inverse(1_KHz)==1_ms);
+  assert(inverse(1_us)==1_MHz);
+  assert(inverse(1_ns)==1_GHz);
+
+  // distance = speed * time
+  Feet ft = 50_mph * 20_mn;
+  cout << "50 MPH for 20 minutes is " << ft << " ft\n";
+  assert(ft == 88000_ft);
+
+  // ostream
+  stringstream ss;
+  ss << ft;
+*/
+  // vector conversion
+  vector<Feet> vf(10);
+  for(int i=0;i<vf.size();i++) vf[i] = Feet(i);
+  vector<Inches> vi(vf.begin(), vf.end());  // convert vector<Feet> to vector<Inches>
+  for(int i=0;i<vi.size();i++) assert(vi[i].get() == vf[i].get<Inches>());
+
+  // algorithm
+  Meters mx = *std::max_element(vf.begin(), vf.end());
+  Meters sum = std::accumulate(vf.begin(), vf.end(), Feet{});
+  cout << "vf max and sum: " << mx << " " << sum << "\n";
+/*
+  // Interunit relationships
+  assert(SquareFeet(1) == 1_ft*1_ft);
+  assert(SquareFeet(1) == 12_in*12_in);
+  assert(Liters(1) == 10_cm*10_cm*10_cm);
+  assert(CubicInches(30)/Inches(10) == SquareInches(3));
+  assert(Acres(1) == 55_yd*88_yd);
+  assert(CubicMeters(1000) == 10_m*10_m*10_m);
+  assert(CubicInches(1000) == 10_in*10_in*10_in);
+  assert((9.80665_m/1_s/1_s) == 1_G);
+  assert((1_G).approx(32.174049_ft/1_s/1_s));
+  assert(1_N == 1_kg*(1_m/1_s/1_s));
+  assert(20_mi/10_hr == 2_mph);
+  assert(20_mi == 2_mph*10_hr);
+  assert(SquareInches(30)/Inches(5) == Inches(6));
+  assert(200_mi/Gallons(10) == MilesPerGallon(20));
+  assert(Rods(40)/HogsHead(1) == RodsToTheHogHead(40));
+  assert(Radians(20)/Seconds(10) == RadiansPerSecond(2));
+  assert(6050_J / 5_us == 1.21_GW);
+  assert(11_mi / 450_s == 88_mph);
+  assert(2_km * 2_km == SquareKilometers(4));
+  assert(sqrt(SquareFeet(144))==12_ft);
+*/
+
+  // trig
+  cout << "sin(180_deg): " << sin(180_deg) << "\n";
+  cout << "cos(180_deg): " << cos(180_deg) << "\n";
+  assert(Degrees(180) == Radians(M_PI));
+  assert(sin(0_deg) == 0);
+  assert(approx(sin(180_deg), sin(M_PI)));
+  assert(sin(90_deg) == 1);
+  assert(cos(0_deg) == 1);
+  assert(approx(cos(90_deg), cos(M_PI/2)));
+  assert(cos(180_deg) == -1);
+  assert(asin_units(0) == 0_deg);
+  assert(asin_units(1) == 90_deg);
+  assert(acos_units(0) == 90_deg);
+  assert(acos_units(1) == 0_deg);
+  assert(atan2_units(2,2) == 45_deg);
+
+  // override default FloatType type
+  Meters mdef(5);
+  assert(Float32<Inches>(24).get<Float32<Feet>>()==2);
+  Float64<Meters> m64(5);
+  Float32<Meters> m32(5);
+  assert(sizeof(mdef)==8);
+  assert(sizeof(m32)==4);
+  assert(sizeof(m64)==8);
+  assert(m32 == mdef);
+  assert(m64 == mdef);
+  Float32<Inches>in = Feet(4);
+  assert(in.get()==48);
+  //auto mphf = Float32<Miles>(10)/Float32<Hours>(5);
+  //assert(sizeof(mphf)==4);
+  //assert(mphf.get()==2);
+
+  // string to unit
+  assert(ToMeters(5,"ft") == 5_ft);
+  assert(ToMeters("5ft") == 5_ft);
+  assert(ToMeters("2.7m") == 2.7_m);
+  assert(ToMeters("  3.14 ft  foo ") == 3.14_ft);
+
+  // extreme example
+  //assert(Angstroms(1)*Angstroms(1)*Angstroms(1)==CubicMeters(1e-30));
+
+  // Smoots and football fields
+  using Smoots = Unit<Distance,1.702L>;
+  assert(Smoots(1).approx(Meters(1.702)));
+  using FootballFields = Unit<Distance, Yards::M*100>;
+  assert(FootballFields(1)==Yards(100));
+
+  // these shouldn't compile
+  //Feet f = 10;               // implicit conversion not allowed
+  //double d = Feet(10);       // implicit conversion not allowed
+  //Miles(1).get<Knots>();     // category mismatch
+  //Miles m2;
+  //m2.set<Knots>(1);          // category mismatch
+  //Miles m = Seconds(1);      // category mismatch
+  //auto s = Meters(1)/Hz(1);  // invalid relationship
+}
+
+#endif
---
+++ units20.h
@@ -0,0 +1,433 @@
+// Copyright (C) 2021 Paul Ilardi (http://github.com/CodePi)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, unconditionally.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+#if __cplusplus < 202000
+#error "C++20 compatible compiler is required for units20.h, use units.h instead"
+#endif
+
+#include <cmath>
+#include <iostream>
+#include <cinttypes>
+
+namespace Units {
+
+///////////////////////////////////////////////////////////////////////////////
+// Unit Class
+
+template<typename Category, long double multiplier, typename FloatType_=double>
+class Unit{
+public:
+
+  //////////////////////
+  // template parameters
+  //typedef Ratio R;
+  typedef Category C;
+  typedef FloatType_ FloatType;
+  typedef FloatType_ FT;
+  static constexpr long double M = multiplier;
+  //static constexpr long double O = Ratio::O;
+
+  //////////////////////////
+  // Constructors and assign
+
+  Unit() = default;
+
+  // Construct from FloatTyoe
+  explicit Unit(FloatType val) : m_val(val) {}
+
+  // Construct from other Unit (and convert)
+  template<long double R2,typename FT2>
+  Unit(Unit<C,R2,FT2> other){
+    set(other);
+  }
+
+  // Assign from other Unit (and convert)
+  template<long double R2,typename FT2>
+  Unit& operator=(Unit<C,R2,FT2> other){
+    set(other);
+    return *this;
+  }
+
+  //////////////
+  // get methods
+
+  // get value as new unit
+  template<typename U>
+  FloatType get() const {
+    return U(*this).get();
+  }
+
+  // get value as current unit
+  FloatType get() const { return m_val; }
+
+  //////////////
+  // set methods
+
+  // set by FloatTyoe value
+  template<typename U>
+  void set(FloatType val){
+    set(U(val));
+  }
+
+  // set by other Unit (and convert)
+  template<long double R2, typename FT2>
+  void set(Unit<C,R2,FT2> other){
+    // Compute multiplier: mult =  R2 / R
+    constexpr long double mult = ((long double)R2 / M);
+    // Compute offset: offset = R_offset - mult * R2_offset
+    constexpr FloatType offset = 0; //TODO((long double)R::O) - mult*((long double)R2::O);
+    // val = mult * other + offset
+    m_val = (FloatType)mult * other.get();
+    if(offset!=0) m_val += offset;
+  }
+
+  void set(FloatType val){
+    m_val = val;
+  }
+
+  ///////////////////////
+  // arithmetic operators
+
+  // Unit = Unit +- Unit
+  template<long double R2, typename FT2> Unit operator+(Unit<C,R2,FT2> other) const{ return Unit(m_val+Unit(other).get()); }
+  template<long double R2, typename FT2> Unit operator-(Unit<C,R2,FT2> other) const{ return Unit(m_val-Unit(other).get()); }
+
+  // Unit = Unit */ FloatType
+  Unit operator*(FloatType scalar) const{ return Unit(m_val*scalar); }
+  Unit operator/(FloatType scalar) const{ return Unit(m_val/scalar); }
+
+  // FloatType = Unit / Unit
+  template<long double R2, typename FT2> FloatType operator/(Unit<C,R2,FT2> other) const{ return m_val / Unit(other).get(); }
+
+  // Unary operators +-
+  Unit operator+() const{ return Unit(m_val); }
+  Unit operator-() const{ return Unit(-m_val); }
+
+  // operators += -= *= /=
+  template<long double R2, typename FT2> Unit& operator+=(Unit<C,R2,FT2> other){ m_val += Unit(other).get(); return *this; }
+  template<long double R2, typename FT2> Unit& operator-=(Unit<C,R2,FT2> other){ m_val -= Unit(other).get(); return *this; }
+  Unit& operator*=(FloatType other)     { m_val *= other; return *this; }
+  Unit& operator/=(FloatType other)     { m_val /= other; return *this; }
+
+  ////////////////////
+  // compare operators
+  template<long double R2, typename FT2> bool operator==(Unit<C,R2,FT2> other) const{ return m_val == Unit(other).get(); }
+  template<long double R2, typename FT2> bool operator!=(Unit<C,R2,FT2> other) const{ return m_val != Unit(other).get(); }
+  template<long double R2, typename FT2> bool operator<=(Unit<C,R2,FT2> other) const{ return m_val <= Unit(other).get(); }
+  template<long double R2, typename FT2> bool operator>=(Unit<C,R2,FT2> other) const{ return m_val >= Unit(other).get(); }
+  template<long double R2, typename FT2> bool operator< (Unit<C,R2,FT2> other) const{ return m_val <  Unit(other).get(); }
+  template<long double R2, typename FT2> bool operator> (Unit<C,R2,FT2> other) const{ return m_val >  Unit(other).get(); }
+
+  template<long double R2, typename FT2>
+  bool approx(Unit<C,R2,FT2> other, double epsilon=1e-6) const{
+    return fabs(m_val-Unit(other).get()) <= epsilon;
+  }
+
+  // allow explicit casts to FloatType (but not implicit)
+  explicit operator FloatType() const {return m_val;};
+
+private:
+  // storage
+  FloatType m_val = 0;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Override default FloatType.  e.g. Float32<Meters> is float instead of double
+template<typename U> using Float32 = Unit<typename U::C, U::M, float>;
+template<typename U> using Float64 = Unit<typename U::C, U::M, double>;
+
+///////////////////////////////////////////////////////////////////////////////
+// Macros
+
+// helper macro for generating user-defined literals for units
+#define GEN_LITERAL(lit, unit)                                              \
+  namespace literals {                                                      \
+  inline unit operator "" lit(long double v){return unit(v);};              \
+  inline unit operator "" lit(unsigned long long v){return unit(v);};       \
+  }
+
+///////////////////////////////////////////////////////////////////////////////
+// categories
+struct Distance{};
+struct Area{};
+struct Volume{};
+struct Time{};
+struct Speed{};
+struct Acceleration{};
+struct Mass{};
+struct Force{};
+struct Momentum{};
+struct Frequency{};
+struct Energy{};
+struct Power{};
+struct Voltage{};
+struct Amperage{};
+struct Resistance{};
+struct Angle{};
+struct AngularVelocity{};
+struct Temperature{};
+struct FuelEfficiency{};
+
+// SI helper
+static constexpr long double Tera  = 1e12L;
+static constexpr long double Giga  = 1e9L;
+static constexpr long double Mega  = 1e6L;
+static constexpr long double Kilo  = 1e3L;
+static constexpr long double Base  = 1e0L;
+static constexpr long double Centi = 1e-2L;
+static constexpr long double Milli = 1e-3L;
+static constexpr long double Micro = 1e-6L;
+static constexpr long double Nano  = 1e-9L;
+
+///////////
+// Distance
+static const long double cm_per_in = 2.54;
+using Kilometers    = Unit<Distance,Kilo>;
+using Meters        = Unit<Distance,Base>;
+using Centimeters   = Unit<Distance,Centi>;
+using Millimeters   = Unit<Distance,Milli>;
+using Microns       = Unit<Distance,Micro>;
+using Nanometers    = Unit<Distance,Nano>;
+using Angstroms     = Unit<Distance,Nano/10>;
+using Inches        = Unit<Distance,2.54L*Centi>;
+using Feet          = Unit<Distance,Inches::M*12>;
+using Yards         = Unit<Distance,Inches::M*12*3>;
+using Furlongs      = Unit<Distance,Inches::M*12*3*220>;
+using Miles         = Unit<Distance,Inches::M*12*3*1760>;
+using NauticalMiles = Unit<Distance,1852.0L>;
+using Rods          = Unit<Distance,Feet::M*16.5>;
+using Bananas       = Unit<Distance,0.178L>;
+
+GEN_LITERAL(_nm, Nanometers)
+GEN_LITERAL(_um, Microns)
+GEN_LITERAL(_mm, Millimeters)
+GEN_LITERAL(_cm, Centimeters)
+GEN_LITERAL( _m, Meters)
+GEN_LITERAL(_km, Kilometers)
+GEN_LITERAL(_in, Inches)
+GEN_LITERAL(_ft, Feet)
+GEN_LITERAL(_yd, Yards)
+GEN_LITERAL(_mi, Miles)
+
+///////
+// Area
+using SquareMeters     = Unit<Area,Base>;
+using SquareInches     = Unit<Area,Inches::M*Inches::M>;
+using SquareFeet       = Unit<Area,Feet::M*Feet::M>;
+using SquareMiles      = Unit<Area,Miles::M*Miles::M>;
+using SquareKilometers = Unit<Area,Kilometers::M*Kilometers::M>;
+using Acres            = Unit<Area,SquareFeet::M*43560>;
+
+/////////
+// Volume
+using Liters           = Unit<Volume,Base>;
+using Milliliters      = Unit<Volume,Milli>;
+using CubicCentimeters = Unit<Volume,Milli>;
+using Microliters      = Unit<Volume,Micro>;
+using CubicMeters      = Unit<Volume,1e3L>;
+using CubicInches      = Unit<Volume,Inches::M*Inches::M*Inches::M*CubicMeters::M>;
+using Gallons          = Unit<Volume,CubicInches::M*231>;
+using Quarts           = Unit<Volume,Gallons::M/4>;
+using Pints            = Unit<Volume,Gallons::M/8>;
+using Cups             = Unit<Volume,Gallons::M/16>;
+using FluidOunces      = Unit<Volume,Gallons::M/128>;
+using HogsHead         = Unit<Volume,Gallons::M*63>;
+
+GEN_LITERAL( _l, Liters)
+GEN_LITERAL(_ml, Milliliters)
+GEN_LITERAL(_ul, Microliters)
+GEN_LITERAL(_cc, CubicCentimeters)
+
+///////
+// Time
+using Seconds      = Unit<Time,Base>;
+using Minutes      = Unit<Time,60.0L>;
+using Hours        = Unit<Time,60.0L*60>;
+using Days         = Unit<Time,60.0L*60*24>;
+using Weeks        = Unit<Time,60.0L*60*24*7>;
+using Fortnights   = Unit<Time,60.0L*60*24*14>;
+using Years        = Unit<Time,Days::M*365.2422>;
+using Milliseconds = Unit<Time,Milli>;
+using Microseconds = Unit<Time,Micro>;
+using Nanoseconds  = Unit<Time,Nano>;
+
+GEN_LITERAL(_wk, Weeks)
+GEN_LITERAL(_dy, Days)
+GEN_LITERAL(_hr, Hours)
+GEN_LITERAL(_mn, Minutes)
+GEN_LITERAL( _s, Seconds)
+GEN_LITERAL(_ms, Milliseconds)
+GEN_LITERAL(_us, Microseconds)
+GEN_LITERAL(_ns, Nanoseconds)
+
+////////
+// Speed
+using MetersPerSecond   = Unit<Speed,Base>;
+using KilometersPerHour = Unit<Speed,1000.0L/3600>;
+using MilesPerHour      = Unit<Speed,Miles::M/Hours::M>;
+using MPH               = Unit<Speed,MetersPerSecond::M>;
+using Knots             = Unit<Speed,1852.0L/3600>;
+using Mach              = Unit<Speed,343.0L>;
+using SpeedOfLight      = Unit<Speed,299792458.0L>;
+
+GEN_LITERAL(_mps, MetersPerSecond)
+GEN_LITERAL(_mph, MilesPerHour)
+GEN_LITERAL(_kph, KilometersPerHour)
+
+/////////////////////////
+// Astronomical Distances
+using LightYears        = Unit<Distance,SpeedOfLight::M*Years::M>;
+using AstronomicalUnits = Unit<Distance,LightYears::M/63241.1>;
+using Parsecs           = Unit<Distance,AstronomicalUnits::M*206265>;
+
+///////////////
+// Acceleration
+using MetersPerSecondSquared = Unit<Acceleration,Base>;
+using FeetPerSecondSquared   = Unit<Acceleration,Feet::M>;
+using Gravity                = Unit<Acceleration,9.80665L>;
+
+GEN_LITERAL(_G, Gravity)
+
+///////
+// Mass
+using MetricTons = Unit<Mass,Mega>;
+using Kilograms  = Unit<Mass,Kilo>;
+using Grams      = Unit<Mass,Base>;
+using Milligrams = Unit<Mass,Milli>;
+using Micrograms = Unit<Mass,Micro>;
+using Pounds     = Unit<Mass,453.59237L>;
+using Ounces     = Unit<Mass,Pounds::M/16>;
+using Tons       = Unit<Mass,Pounds::M*2000>;
+using Stones     = Unit<Mass,Pounds::M*14>;
+
+GEN_LITERAL(_kg, Kilograms)
+GEN_LITERAL( _g, Grams)
+GEN_LITERAL(_mg, Milligrams)
+GEN_LITERAL(_ug, Micrograms)
+GEN_LITERAL(_lbs, Pounds)
+GEN_LITERAL(_oz, Ounces)
+
+/////////////////////
+// Force and momentum
+using Newtons                  = Unit<Force,Base>;
+using PoundsForce              = Unit<Force,Pounds::M*Gravity::M/1000>;
+using KilogramsMetersPerSecond = Unit<Momentum,Base>;
+GEN_LITERAL(_N, Newtons);
+
+/////////
+// Energy
+using Gigawatts  = Unit<Power,Giga>;
+using Megawatts  = Unit<Power,Mega>;
+using Kilowatts  = Unit<Power,Kilo>;
+using Watts      = Unit<Power,Base>;
+using Milliwatts = Unit<Power,Milli>;
+
+GEN_LITERAL(_GW, Gigawatts);
+GEN_LITERAL(_MW, Megawatts);
+GEN_LITERAL(_KW, Kilowatts);
+GEN_LITERAL( _W, Watts);
+GEN_LITERAL(_mW, Milliwatts);
+
+////////
+// Power
+using Megajoules    = Unit<Energy,Mega>;
+using Kilojoules    = Unit<Energy,Kilo>;
+using Joules        = Unit<Energy,Base>;
+using WattHours     = Unit<Energy,Hours::M>;
+using KilowattHours = Unit<Energy,Kilo*WattHours::M>;
+using MegawattHours = Unit<Energy,Mega*WattHours::M>;
+
+GEN_LITERAL(_MWh, MegawattHours)
+GEN_LITERAL(_KWh, KilowattHours)
+GEN_LITERAL( _Wh, WattHours)
+GEN_LITERAL(_MJ,  Megajoules)
+GEN_LITERAL(_KJ,  Kilojoules)
+GEN_LITERAL( _J,  Joules)
+
+//////////////
+// Electricity
+using Kilovolts  = Unit<Voltage,Kilo>;
+using Volts      = Unit<Voltage,Base>;
+using Millivolts = Unit<Voltage,Milli>;
+using Amps       = Unit<Amperage,Base>;
+using Milliamps  = Unit<Amperage,Milli>;
+using Kiloohms   = Unit<Resistance,Kilo>;
+using Ohms       = Unit<Resistance,Base>;
+using Milliohms  = Unit<Resistance,Milli>;
+
+GEN_LITERAL(_KV, Kilovolts)
+GEN_LITERAL(_V, Volts)
+GEN_LITERAL(_mV, Millivolts)
+GEN_LITERAL(_A, Amps)
+GEN_LITERAL(_mA, Milliamps)
+GEN_LITERAL(_ohms, Ohms)
+
+//////////////////
+// Fuel Efficiency
+using KilometersPerLiter = Unit<FuelEfficiency, Base>;
+using MilesPerGallon     = Unit<FuelEfficiency, Miles::M/Gallons::M/1000>;
+using RodsToTheHogHead   = Unit<FuelEfficiency, Rods::M/HogsHead::M/1000>;
+
+GEN_LITERAL(_mpg, MilesPerGallon);
+
+////////////
+// Frequency
+using  Hz = Unit<Frequency,Base>;
+using KHz = Unit<Frequency,Kilo>;
+using MHz = Unit<Frequency,Mega>;
+using GHz = Unit<Frequency,Giga>;
+using THz = Unit<Frequency,Tera>;
+
+GEN_LITERAL( _Hz,  Hz)
+GEN_LITERAL(_KHz, KHz)
+GEN_LITERAL(_MHz, MHz)
+GEN_LITERAL(_GHz, GHz)
+GEN_LITERAL(_THz, THz)
+
+////////
+// Angle
+#ifndef M_PIl  // not defined on some systems
+#define M_PIl ((long double)M_PI)
+#endif
+using Radians = Unit<Angle,Base>;
+using Degrees = Unit<Angle,M_PIl/180>;
+using RadiansPerSecond = Unit<AngularVelocity,Base>;
+using DegreesPerSecond = Unit<AngularVelocity,M_PIl/180>;
+GEN_LITERAL(_rad, Radians)
+GEN_LITERAL(_deg, Degrees)
+
+// trig functions
+double sin(Radians r) { return std::sin(r.get()); }
+double cos(Radians r) { return std::cos(r.get()); }
+double tan(Radians r) { return std::tan(r.get()); }
+// Note: using _units here to avoid collision with std namespace since they both take double as arg
+Radians asin_units(double v) { return Radians(std::asin(v)); }
+Radians acos_units(double v) { return Radians(std::acos(v)); }
+Radians atan_units(double v) { return Radians(std::atan(v)); }
+Radians atan2_units(double v1, double v2) { return Radians(std::atan2(v1,v2)); }
+
+///////////////////////////////////////////////////////////////////////////////
+// Stream operator
+template <typename C,long double R,typename FloatType>
+inline std::ostream& operator<<(std::ostream&os, const Unit<C,R,FloatType>& u){
+  os << u.get();
+  return os;
+}
+
+}  // end namespace Units
